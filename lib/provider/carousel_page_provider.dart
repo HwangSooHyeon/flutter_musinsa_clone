c72e54e09d981b55e45f260634e6f3ee7fa381d0
amb <mikaelh1651@gmail.com>
2023-03-08 19:47:23 +0900

    fix(carousel_page_provider): cache 추가

--- lib/provider/carousel_page_provider.dart
+++ lib/provider/carousel_page_provider.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_musinsa_clone/repository/carousel_page_repository.dart';
 
+import '../model/carousel_page.dart';
+
 class CarouselPageProvider extends ChangeNotifier {
   final CarouselPageRepository repository;
+
+  Map<String, List<CarouselPage>> cache = {};
 
   CarouselPageProvider({
     required this.repository,
   });
 
-  void getCarouselPages({required String companyId}) async {
+  void getCarouselPages({
+    required String companyId,
+  }) async {
     final response = await repository.getCarouselPages(companyId: companyId);
+    cache.update(companyId, (value) => response, ifAbsent: () => response);
     notifyListeners();
   }
 }
