89bfed5d6cca639dbdbbb976656a200bfa207924
amb <mikaelh1651@gmail.com>
2023-03-08 18:46:26 +0900

    feat(carousel_page_repository): Carousel 페이지 repository 정의

---
+++ lib/repository/carousel_page_repository.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_musinsa_clone/model/carousel_page.dart';
+
+class CarouselPageRepository {
+  final _dio = Dio();
+  final _targetUrl =
+      'http://${Platform.isAndroid ? '10.0.2.2' : 'localhost'}:3000/carouselpage';
+
+  Future<List<CarouselPage>> getCarouselPages(
+      {required String companyId}) async {
+    final reaponse = await _dio.get(
+      _targetUrl,
+      queryParameters: {
+        'companyId': companyId,
+      },
+    );
+    return reaponse.data
+        .map<CarouselPage>(
+          (x) => CarouselPage.fromJson(
+            json: x,
+          ),
+        )
+        .toList();
+  }
+}
