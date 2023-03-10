8c3ee389ec244334aff947f95bf010692c684c57
amb <mikaelh1651@gmail.com>
2023-03-10 19:25:56 +0900

    feat(main): provider 사용을 위한 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_musinsa_clone/provider/carousel_page_provider.dart';
+import 'package:flutter_musinsa_clone/repository/carousel_page_repository.dart';
 import 'package:flutter_musinsa_clone/screen/home_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final carouselPageRepository = CarouselPageRepository();
+  final carouselPageProvider = CarouselPageProvider(repository: carouselPageRepository);
+
   runApp(
-    MaterialApp(
-        home: HomeScreen(),
+    ChangeNotifierProvider(
+      create: (_) => carouselPageProvider,
+      child: MaterialApp(
+          home: HomeScreen(),
+      ),
     ),
   );
 }
