c2fa2bbbbffd009ca4b6bd7c4ce4137c4e6b8bb7
amb <mikaelh1651@gmail.com>
2023-03-12 00:23:54 +0900

    test(carousel_page): json 수신 테스트 expect 메서드를 이용해 테스트 하도록 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_musinsa_clone/provider/carousel_page_provider.dart';
 import 'package:flutter_musinsa_clone/repository/carousel_page_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   final carouselPageRepository = CarouselPageRepository();
-  final carouselPageProvider = CarouselPageProvider(repository: carouselPageRepository);
 
-  test('json 전송 테스트', () async {
+  test('json 수신 테스트', () async {
     final response =
         await carouselPageRepository.getCarouselPages(companyId: 'musinsa');
-    for (var element in response) {
-        print(element.toString());
-      }
+    List<String> actualData = [];
+    for(int i = 0; i < response.length; i++) {
+      actualData.add(response[i].toString());
+    }
+    expect(actualData, [
+      'id: 1, imgPath: imgPath1, companyId: musinsa',
+      'id: 2, imgPath: imgPath2, companyId: musinsa',
+      'id: 3, imgPath: imgPath3, companyId: musinsa',
+    ]);
   });
 }
