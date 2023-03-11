e242bc9d52b41dbe7eed4d41475123f83e725d73
amb <mikaelh1651@gmail.com>
2023-03-11 17:15:30 +0900

    test(widget_test): json 수신 테스트 코드 작성

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,18 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_musinsa_clone/provider/carousel_page_provider.dart';
+import 'package:flutter_musinsa_clone/repository/carousel_page_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_musinsa_clone/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  final carouselPageRepository = CarouselPageRepository();
+  final carouselPageProvider = CarouselPageProvider(repository: carouselPageRepository);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('json 전송 테스트', () async {
+    final response =
+        await carouselPageRepository.getCarouselPages(companyId: 'musinsa');
+    for (var element in response) {
+        print(element.toString());
+      }
   });
 }
