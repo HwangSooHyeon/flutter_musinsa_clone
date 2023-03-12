069f63c6f7f34a9a7c8df33fa8542ef8a089c0b3
amb <mikaelh1651@gmail.com>
2023-03-12 16:56:50 +0900

    feat(recommendation_screen): 추천 스크린에 그리드 뷰 적용

--- lib/screen/recommendation_screen.dart
+++ lib/screen/recommendation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_musinsa_clone/component/custom_icon_button.dart';
 
 import '../component/main_carousel.dart';
 
@@ -12,10 +13,7 @@ class RecommendationScreen extends StatelessWidget {
     return Wrap(
       children: [
         MainCarousel(),
-        Container(
-          height: 1000,
-          color: Colors.amber,
-        ),
+        CustomIconButton(),
         Container(
           height: 1000,
           color: Colors.amberAccent,
