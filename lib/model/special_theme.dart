a1d0fdad241216464221aa36317e03794c877ea6
amb <mikaelh1651@gmail.com>
2023-03-12 15:34:15 +0900

    feat(special_theme): model에 label 속성 추가

--- lib/model/special_theme.dart
+++ lib/model/special_theme.dart
@@ -1,11 +1,13 @@
 class SpecialTheme {
   final String id;
   final String themeName;
+  final String label;
   final String content;
 
   SpecialTheme({
     required this.id,
     required this.themeName,
+    required this.label,
     required this.content,
   });
 
@@ -13,10 +15,11 @@ class SpecialTheme {
     required Map<String, dynamic> json,
   })  : id = json['id'],
         themeName = json['themeName'],
+        label = json['label'],
         content = json['content'];
 
   @override
   String toString() {
-    return 'id: $id, themeName: $themeName, content: $content';
+    return 'id: $id, themeName: $themeName, label: $label, content: $content';
   }
 }
