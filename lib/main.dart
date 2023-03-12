5895b841153c7f7b824cacdf6db37eee642c7fea
amb <mikaelh1651@gmail.com>
2023-03-12 16:53:40 +0900

    feat(main): special theme repository, provider 선언 및 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_musinsa_clone/provider/carousel_page_provider.dart';
+import 'package:flutter_musinsa_clone/provider/special_theme_provider.dart';
 import 'package:flutter_musinsa_clone/repository/carousel_page_repository.dart';
+import 'package:flutter_musinsa_clone/repository/special_theme_repository.dart';
 import 'package:flutter_musinsa_clone/screen/home_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final carouselPageRepository = CarouselPageRepository();
+  final specialThemeRepository = SpecialThemeRepository();
+
   final carouselPageProvider =
       CarouselPageProvider(repository: carouselPageRepository);
+  final specialThemeProvider =
+      SpecialThemeProvider(repository: specialThemeRepository);
 
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => carouselPageProvider,
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => carouselPageProvider),
+        ChangeNotifierProvider(create: (_) => specialThemeProvider),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: HomeScreen(),
