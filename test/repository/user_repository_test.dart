c823f98f7ba90002e94d7222018229894bcb17bc
amb <mikaelh1651@gmail.com>
2023-03-22 18:15:44 +0900

    test(user_repository_test): repository 코드를 적용하기 위한 리팩터링 및 계정 생성 테스트 코드 작성

--- test/repository/user_repository_test.dart
+++ test/repository/user_repository_test.dart
@@ -6,16 +6,27 @@ import 'package:flutter_test/flutter_test.dart';
 Future<void> main() async {
   final instance = FakeFirebaseFirestore();
   final user = User(
-      id: 'mikaelh',
-      password:
-          '99ff168a7df53d9d57010acad9ebcecbd06e6e77ee0503e4a2e8255063da251e',
-      email: 'mikaelh@naver.com');
-  await instance.collection('user').doc(user.id).set(user.toJson());
-  final userRepository = UserRepository();
+    id: 'mikaelh',
+    password:
+        '99ff168a7df53d9d57010acad9ebcecbd06e6e77ee0503e4a2e8255063da251e',
+    email: 'mikaelh@naver.com',
+  );
+  final userRepository = UserRepository(instance: instance);
+
+  test('firestore 전송 테스트', () async {
+    final response = await userRepository.createUser(
+      user: user,
+    );
+    expect(response, isTrue);
+  });
 
   test('firestore 수신 테스트', () async {
-    final response =
-        await userRepository.getUser(id: 'mikaelh', instance: instance);
+    await userRepository.createUser(
+      user: user,
+    );
+    final response = await userRepository.getUser(
+      id: 'mikaelh',
+    );
     expect(response.toString(),
         'id: mikaelh, password: 99ff168a7df53d9d57010acad9ebcecbd06e6e77ee0503e4a2e8255063da251e, email: mikaelh@naver.com');
   });
