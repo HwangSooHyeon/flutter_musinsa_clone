1bbb63ea8e3f7d68721273b14a612e08a9d374db
amb <mikaelh1651@gmail.com>
2023-03-22 18:08:45 +0900

    feat(user_repository): createUser 메서드 정의 및 Firestore instance를 생성자에서 주입받도록 변경

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -3,8 +3,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../model/user.dart';
 
 class UserRepository {
-  Future<User?> getUser(
-      {required String id, required FirebaseFirestore instance}) async {
+  final FirebaseFirestore instance;
+
+  UserRepository({
+    required this.instance,
+  });
+
+  Future<User?> getUser({
+    required String id,
+  }) async {
     final response = await instance.collection('user').doc(id).get();
     if (response.data() == null) {
       return null;
@@ -14,5 +21,17 @@ class UserRepository {
       password: response.data()?['password'] ?? '',
       email: response.data()?['email'] ?? '',
     );
+  }
+
+  Future<bool> createUser({
+    required User user,
+  }) async {
+    try {
+      await instance.collection('user').doc(user.id).set(user.toJson());
+      return true;
+    } catch (e) {
+      print('계정 생성 중 에러 발생: $e');
+      return false;
+    }
   }
 }
